d6e5f1f8757e738e0c7a5ce067bd000f8c93e658
leroyds <leroy.saldanha@turtlemint.com>
2024-05-16 00:09:17 +0530

    close icon

--- src/components/FilterLabel/FilterLabel.jsx
+++ src/components/FilterLabel/FilterLabel.jsx
@@ -1,4 +1,5 @@
-import './filter-label.scss'
+import './filter-label.scss';
+import CloseIcon from '@mui/icons-material/Close';
 
 const FilterLabel = ({filterCheckOn, filters, label, clearFilter, isSearch, search}) => {
 
@@ -8,14 +9,14 @@ const FilterLabel = ({filterCheckOn, filters, label, clearFilter, isSearch, sear
                 filterCheckOn && !isSearch &&
                 <div className="filter-label">
                     {filters[label].title}
-                    <span onClick={() => clearFilter(label)}>x</span>
+                    <span onClick={() => clearFilter(label)}><CloseIcon/></span>
                 </div>
             }
             {
                 filterCheckOn && isSearch && search &&
                 <div className="filter-label">
                     {search}
-                    <span onClick={() => clearFilter('search')}>x</span>
+                    <span onClick={() => clearFilter('search')}><CloseIcon/></span>
                 </div>
             }
         </>
